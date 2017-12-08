8ace5c05af09bd0a8f2e47a68761fb3607070654
DrPunpun <34072827+DrPunpun@users.noreply.github.com>
2017-12-08 18:53:57 +0000

    Add files via upload

---
+++ Team.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    var vm = function () {
+        console.log('ViewModel initiated...')
+        //---Variáveis locais
+        var self = this;
+        var baseUri = 'http://192.168.160.28/football/api/teams';
+        self.error = ko.observable();
+        self.clubTeam = ko.observableArray([]);
+        //--- Internal functions
+        function ajaxHelper(uri, method, data) {
+            self.error(''); // Clear error message
+            return $.ajax({
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: data ? JSON.stringify(data) : null,
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("AJAX Call[" + uri + "] Fail...");
+                    self.error(errorThrown);
+                }
+            })
+        }
+        //--- Externel functions (accessible outside)
+        teams = function () {
+            var baseUri = 'http://192.168.160.28/football/api/teams'
+            ajaxHelper(baseUri, 'GET').done(function (data) {
+                self.clubTeam(data);
+            });
+        }
+        teams()
+
+        search = function () {
+            searchTeam = $('#clubTeam1').val()
+            baseUri = 'http://192.168.160.28/football/api/teams/search?srcStr=' + searchTeam
+            ajaxHelper(baseUri, 'GET').done(function (data) {
+                self.clubTeam(data);
+            });
+        };
+        del = function () {
+            $('#clubTeam1').val('');
+            teams()
+        }
+
+        };
+        ko.applyBindings(new vm())
+    })
---
+++ team-details.js
@@ -0,0 +1,37 @@
+﻿$('document').ready(function () {
+    var vm = function () {
+        console.log('ViewModel initiated...')
+        //---Variáveis locais
+        var url = location.hash;
+        var list_ids = url.split('/');
+        var fifa_id = list_ids.pop();
+        var id = list_ids.pop();
+        document.getElementById('teamImage').src = 'https://cdn.sofifa.org/18/teams/' + fifa_id + '.png';
+        var self = this;
+        var baseUri = 'http://192.168.160.28/football/api/teams/seasons/' + id;
+        self.error = ko.observable();
+        self.teamInfo = ko.observableArray([]);
+        //--- Internal functions
+        function ajaxHelper(uri, method, data) {
+            self.error(''); // Clear error message
+            return $.ajax({
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: data ? JSON.stringify(data) : null,
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("AJAX Call[" + uri + "] Fail...");
+                    self.error(errorThrown);
+                }
+            })
+        }
+        getInfo = function () {
+            ajaxHelper(baseUri, 'GET').done(function (data) {
+                self.teamInfo(data);
+            });
+        };
+        getInfo()
+    }
+    ko.applyBindings(new vm)
+})
